d9d550f666aaf6c0856dbcdc9952d69042fe8a29
luchanos <lukash_91@mail.ru>
2022-12-01 23:08:40 +0300

    test for deletion

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from main import app
 from fastapi.testclient import TestClient
 
@@ -21,7 +23,7 @@ async def create_document(test_db, test_elastic, document_id, rubrics, text, cre
 async def test_create_document(test_db, test_elastic):
     text = "sample text"
     rubrics = ["python", "programming", "backend"]
-    date_string = "2032-04-23"
+    date_string = "2032-11-23"
     time_string = "10:20:30"
     created_date_string = f"{date_string}T{time_string}"
     document_data = {
@@ -52,20 +54,23 @@ async def test_create_document(test_db, test_elastic):
     assert document_from_elastic["Id"] == document_id
 
 
-#todo luchanos дописать
 async def test_delete_document(test_db, test_elastic):
     text = "sample text"
     rubrics = ["python", "programming", "backend"]
-    created_date = "2022-01-01"
+    year, month, day, hour, minute, second = 2032, 11, 23, 10, 20, 30
+    date_string = f"{year}-{month}-{day}"
+    time_string = f"{hour}:{minute}:{second}"
+    created_date_string = f"{date_string} {time_string}"
+    date_string = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute, second=second)
     document_data = {
         "text": text,
         "rubrics": rubrics,
-        "created_date": created_date
+        "created_date": date_string
     }
 
-    await create_document(test_db, test_elastic, document_id=1, **document_data)
+    document_id = await create_document(test_db, test_elastic, document_id=1, **document_data)
     with TestClient(app) as client:
-        res = client.post("/create_document", json=document_data)
+        res = client.delete(f"/delete_document?document_id={document_id}")
         resp = res.json()
         assert res.status_code == 200
         assert resp["success"]
@@ -77,13 +82,9 @@ async def test_delete_document(test_db, test_elastic):
         returned_document = document_returned_data[0]
         assert returned_document["id"] == document_id
         assert returned_document["rubrics"] == rubrics
-        assert str(returned_document["created_date"]) == created_date
+        assert str(returned_document["created_date"]) == created_date_string
         assert returned_document["text"] == text
+        assert returned_document["is_deleted"]
 
     documents_from_elastic = await test_elastic.search(index="documents", query={"match": {"Id": document_id}})
-    assert len(documents_from_elastic.body["hits"]["hits"]) == 1
-    document_from_elastic = documents_from_elastic["hits"]["hits"][0]["_source"]
-    assert document_from_elastic["created_date"] == created_date
-    assert document_from_elastic["text"] == text
-    assert document_from_elastic["rubrics"] == rubrics
-    assert document_from_elastic["Id"] == document_id
+    assert len(documents_from_elastic.body["hits"]["hits"]) == 0
